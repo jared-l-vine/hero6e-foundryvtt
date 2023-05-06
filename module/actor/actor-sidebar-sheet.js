b8aaa29766715a24ebdba6dc23c8d2bbd869d87d
aeauseth <aaron.auseth@comcast.net>
2023-05-05 21:00:59 -0700

    sidebar nav is working with scrollable content

--- module/actor/actor-sidebar-sheet.js
+++ module/actor/actor-sidebar-sheet.js
@@ -7,7 +7,7 @@ export class HeroSystem6eActorSidebarSheet extends ActorSheet {
       template: "systems/hero6efoundryvttv2/templates/actor-sidebar/actor-sidebar-sheet.hbs",
       //width: 600,
       //height: 600,
-      //tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "features" }],
+      tabs: [{ navSelector: ".sheet-navigation", contentSelector: ".sheet-body", initial: "Attacks" }],
     });
   }
 
